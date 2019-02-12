04c9d80a5e5aace997cc4fc764ea75a61e1736e9
sambapython@gmail.com <sambapython@gmail.com>
2019-02-12 09:29:07 +0530

    2018-02-12 folder as a module.

--- modules/main.py
+++ modules/main.py
@@ -10,8 +10,27 @@ print(b)
 print(c)
 fun()
 '''
+'''
 import sys
 print(sys.path)
 import file1
 file1.fun()
 print(file1.a,file1.b,file1.c)
+'''
+'''
+import sys
+#sys.path.append("/home/khyaathipython/")
+sys.path.insert(1,"/home/khyaathipython/")
+print(sys.path)
+import file2
+'''
+#import folder1
+#import folder1
+#folder1.f1.fun()
+'''
+from folder1 import f1,f2
+f1.fun()
+f2.fun()
+'''
+from folder1 import f1
+f1.fun()
---
+++ modules/main1.py
@@ -0,0 +1,8 @@
+'''
+import folder1
+print(folder1.f2.fun())
+'''
+'''
+from folder1 import f2
+f2.fun()
+'''
